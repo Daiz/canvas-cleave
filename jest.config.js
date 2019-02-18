5f7c7d7eb06818aaa9c80656b0b493d1fe90fed2
Daiz <taneli.vatanen@gmail.com>
2019-02-18 19:16:00 +0200

    Add code coverage collection npm script

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   },
   testEnvironment: "node",
   testRegex: "\\.test\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["src/**/*.tsx"]
 };
