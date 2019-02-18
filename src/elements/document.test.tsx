42d3bf756be6ff5abbe7bf1ca529c9437dd51973
Daiz <taneli.vatanen@gmail.com>
2019-02-18 17:39:05 +0200

    Fully document and test NodeDocument

--- src/elements/document.test.tsx
+++ src/elements/document.test.tsx
@@ -1,8 +1,39 @@
 import { NodeCanvas } from "./canvas";
 import { NodeDocument } from "./document";
 
+interface DocumentGlobal extends NodeJS.Global {
+  document?: any;
+}
+declare const global: DocumentGlobal;
+
 test(`document.createElement("canvas") should return a NodeCanvas object`, () => {
   const document = new NodeDocument();
   const canvas = document.createElement("canvas");
   expect(canvas).toBeInstanceOf(NodeCanvas);
+});
+
+test(`NodeDocument.inject() should inject an instance of NodeDocument into the global.document key`, () => {
+  expect(global.document).toBe(undefined);
+  NodeDocument.inject();
+  expect(global.document instanceof NodeDocument).toBe(true);
+});
+
+test(`NodeDocument.inject() should do nothing if global.document exists already`, () => {
+  NodeDocument.inject();
+  const document = global.document;
+  NodeDocument.inject();
+  expect(global.document).toBe(document);
+});
+
+test(`NodeDocument.eject() should undefine global.document if it's an instance of NodeDocument`, () => {
+  NodeDocument.inject();
+  NodeDocument.eject();
+  expect(global.document).toBe(undefined);
+});
+
+test(`NodeDocument.eject() should do nothing if global.document exists and is not an instance of NodeDocument`, () => {
+  const obj = {};
+  global.document = obj;
+  NodeDocument.eject();
+  expect(global.document).toBe(obj);
 });
--- src/elements/document.tsx
+++ src/elements/document.tsx
@@ -7,15 +7,30 @@ interface DocumentGlobal extends NodeJS.Global {
 declare const global: DocumentGlobal;
 
 /**
+ * A minimal Node implementation for the DOM Document object to enable cross-environment creation of `canvas` elements using `document.createElement("canvas")`.
  * @public
  */
 export class NodeDocument implements IDocument {
+  /**
+   * Create an element. Only "canvas" is supported by this implementation.
+   * @param el The name of the element to be created. Must be "canvas".
+   */
   createElement(el: "canvas"): NodeCanvas {
     return new NodeCanvas();
   }
+
+  /**
+   * Injects an instance of NodeDocument into the global object as "document" if one doesn't exist already (ie. it does nothing in the browser nor on subsequent calls).
+   * @public
+   */
   static inject(): void {
     global.document = global.document || new NodeDocument();
   }
+
+  /**
+   * Reverses the NodeDocument injection from {@link NodeDocument.inject} by undefining the global.document variable if it's an instance of {@link NodeDocument}.
+   * @public
+   */
   static eject(): void {
     if (global.document instanceof NodeDocument) {
       global.document = undefined;
