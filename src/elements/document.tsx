c1b48d7fa89b3eac43f2a6809c874d7ba68d7eee
Daiz <taneli.vatanen@gmail.com>
2019-02-18 17:38:54 +0200

    Add static inject/eject methods to NodeDocument

--- src/elements/document.tsx
+++ src/elements/document.tsx
@@ -1,11 +1,24 @@
 import { IDocument } from "../interfaces";
 import { NodeCanvas } from "./canvas";
 
+interface DocumentGlobal extends NodeJS.Global {
+  document?: NodeDocument;
+}
+declare const global: DocumentGlobal;
+
 /**
  * @public
  */
 export class NodeDocument implements IDocument {
   createElement(el: "canvas"): NodeCanvas {
     return new NodeCanvas();
+  }
+  static inject(): void {
+    global.document = global.document || new NodeDocument();
+  }
+  static eject(): void {
+    if (global.document instanceof NodeDocument) {
+      global.document = undefined;
+    }
   }
 }
