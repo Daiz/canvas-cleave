e7a9e1358341d96a70fb0f8d54b10b2f8486b064
Daiz <taneli.vatanen@gmail.com>
2019-04-20 15:31:48 +0300

    Add NIRawImage test data to test/images

--- test/images.tsx
+++ test/images.tsx
@@ -1,4 +1,15 @@
 import { resolve } from "path";
+import { NIRawImage } from "../src";
+
+const mock: NIRawImage = {
+  info: {
+    format: "raw",
+    width: 2,
+    height: 1,
+    channels: 4
+  },
+  data: Buffer.from([255, 255, 255, 255, 0, 0, 0, 255])
+};
 
 const images = {
   redYellow: resolve(__dirname, "images/red-yellow.png"),
@@ -7,7 +18,8 @@ const images = {
   green: resolve(__dirname, "images/green.png"),
   greenSticker: resolve(__dirname, "images/green-sticker.png"),
   sticker: resolve(__dirname, "images/sticker.png"),
-  yellowGreenWithSticker: resolve(__dirname, "images/yellow-green+sticker.png")
+  yellowGreenWithSticker: resolve(__dirname, "images/yellow-green+sticker.png"),
+  rawImage: mock
 };
 
 export default images;
