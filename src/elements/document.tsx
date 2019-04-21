99742fc30926db07dfe5d2de29a826af5d3cab1d
Daiz <taneli.vatanen@gmail.com>
2019-04-21 18:07:21 +0300

    Fix missing hyphen in document documentation

--- src/elements/document.tsx
+++ src/elements/document.tsx
@@ -13,7 +13,7 @@ declare const global: DocumentGlobal;
 export class NodeDocument implements IDocument {
   /**
    * Create an element. Only "canvas" is supported by this implementation.
-   * @param el The name of the element to be created. Must be "canvas".
+   * @param el - The name of the element to be created. Must be "canvas".
    */
   createElement(el: "canvas"): NodeCanvas {
     return new NodeCanvas();
